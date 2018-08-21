99f8a486841174c6c8c608d5308f4f94446a122d
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-21 19:56:57 +0100

    implement navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 // components
 import LinkList from './LinkList.component';
 import CreateLink from './CreateLink.component';
+import Header from './Header.component';
 
 // styles
 import '../styles/App.css';
 
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <CreateLink />
-        <LinkList />
-      </Fragment>
-    );
-  }
-}
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter } from 'react-router-dom';
 
 import './styles/index.css';
 import App from './components/App';
@@ -8,7 +9,13 @@ import registerServiceWorker from './registerServiceWorker';
 import client from './utils/ApolloClient';
 
 
-ReactDOM.render(<ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>, document.getElementById('root'));
+const Main = () => (
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
