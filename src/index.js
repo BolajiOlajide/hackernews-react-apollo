afe21e86bfe1a3e3eee367889ecaa796f765ef32
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-21 08:55:42 +0100

    configure apollo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import client from './utils/ApolloClient';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
