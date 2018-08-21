e3224e974fce8c00b5fc20b9e803c64d02e44244
Bolaji Olajide <bolaji.olajide@andela.com>
2018-08-22 00:49:10 +0100

    implement subscription

--- src/components/LinkList.component.jsx
+++ src/components/LinkList.component.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo'
 
-// graphql queries
+// graphql stuffs
 import { FEED_QUERY } from '../queries/link.query';
+import { NEW_LINKS_SUBSCRIPTION } from '../subscriptions/link.subscription';
+import { NEW_VOTES_SUBSCRIPTION } from '../subscriptions/vote.subscription';
 
 // components
 import Link from './Link.component';
@@ -18,13 +20,39 @@ class LinkList extends Component {
       store.writeQuery({ query: FEED_QUERY, data })
     }
 
+    _subscribeToNewVotes = subscribeToMore => {
+      subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
+      });
+    }
+
+    _subscribeToNewLinks = subscribeToMore => {
+      subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const newLink = subscriptionData.data.newLink.node;
+
+          return Object.assign({}, prev, {
+            feed: {
+              links: [newLink, ...prev.feed.links],
+              count: prev.feed.links.length + 1,
+              __typename: prev.feed.__typename
+            }
+          });
+        }
+      });
+    }
+
     render() {
       return (
         <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <div>Fetching</div>
-            if (error) return <div>Error: {error.message}</div>
+          {({ loading, error, data, subscribeToMore }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error: {error.message}</div>;
 
+            this._subscribeToNewLinks(subscribeToMore);
+            this._subscribeToNewVotes(subscribeToMore);
             const linksToRender = data.feed.links;
 
             return (
@@ -33,7 +61,7 @@ class LinkList extends Component {
                   return  <Link key={link.id} link={link} index={index} updateStoreAfterVote={this._updateCacheAfterVote} />
                 })}
               </div>
-            )
+            );
           }}
         </Query>
       )
--- src/utils/ApolloClient.js
+++ src/utils/ApolloClient.js
@@ -2,10 +2,23 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 // utils
 import { AUTH_TOKEN } from './constants';
 
+
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+});
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem(AUTH_TOKEN);
@@ -21,8 +34,17 @@ const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 });
 
